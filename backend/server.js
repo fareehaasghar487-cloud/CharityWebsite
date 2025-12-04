2c720877740a971677832ea01377ad6708c87338
fareehaasghar487-cloud <fareehaasghar487@gmail.com>
2025-12-04 15:43:57 +0500

    push

--- backend/server.js
+++ backend/server.js
@@ -10,10 +10,11 @@ const PORT = 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors())
 
 // Database connection
 mongoose
-  .connect("mongodb://127.0.0.1:27017/backend")
+  .connect("mongodb+srv://fareehaasghar487_db_user:charity@cluster0.asnuo9t.mongodb.net")
   .then(() => console.log("✅ Database is connected successfully"))
   .catch((err) => console.log("❌ Error in connecting database", err));
 
